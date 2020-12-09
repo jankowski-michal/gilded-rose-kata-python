126f1f8cdc3c8f4bfbc10896bcc16f10614d7dce
Michał Jankowski <orbite.mobile@gmail.com>
2020-12-09 18:53:10 +0100

    Tests refactor to match objects structure

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -3,9 +3,12 @@ import unittest
 
 from gilded_rose import Item, GildedRose, BACKSTAGE_PASS, AGED_BRIE, SULFURAS
 
-CONJURED_MANA_CAKE = 'Conjured Mana Cake'
-ELIXIR_OF_THE_MONGOOSE = 'Elixir of the Mongoose'
-DEXTERITY_VEST = '+5 Dexterity Vest'
+
+def create_item_and_update_quality(name, sell_in, quality):
+    items = [Item(name, sell_in, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    return items[0]
 
 
 class GildedRoseTest(unittest.TestCase):
@@ -15,19 +18,19 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual('foo', items[0].name)
 
-    def create_item_and_update_quality(self, name, sell_in, quality):
-        items = [Item(name, sell_in, quality)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        return items[0]
-
     def assert_item_sell_in(self, expected, name, sell_in, quality):
-        item = self.create_item_and_update_quality(name, sell_in, quality)
+        item = create_item_and_update_quality(name, sell_in, quality)
         self.assertEqual(expected, item.sell_in)
 
     def assert_item_quality(self, expected, name, sell_in, quality):
-        item = self.create_item_and_update_quality(name, sell_in, quality)
+        item = create_item_and_update_quality(name, sell_in, quality)
         self.assertEqual(expected, item.quality)
+
+    def assert_generic_quality(self, expected, sell_in, quality):
+        self.assert_item_quality(expected, 'A generic name', sell_in, quality)
+
+    def assert_generic_sell_in(self, expected, sell_in, quality):
+        self.assert_item_sell_in(expected, 'A generic name', sell_in, quality)
 
     def assert_backstage_passes_quality(self, expected, sell_in, quality):
         self.assert_item_quality(expected, BACKSTAGE_PASS, sell_in, quality)
@@ -40,12 +43,6 @@ class GildedRoseTest(unittest.TestCase):
 
     def assert_brie_sell_in(self, expected, sell_in, quality):
         self.assert_item_sell_in(expected, AGED_BRIE, sell_in, quality)
-
-    def assert_elixir_quality(self, expected, sell_in, quality):
-        self.assert_item_quality(expected, ELIXIR_OF_THE_MONGOOSE, sell_in, quality)
-
-    def assert_elixir_sell_in(self, expected, sell_in, quality):
-        self.assert_item_sell_in(expected, ELIXIR_OF_THE_MONGOOSE, sell_in, quality)
 
     def assert_sulfuras_quality(self, expected, sell_in, quality):
         self.assert_item_quality(expected, SULFURAS, sell_in, quality)
@@ -73,22 +70,16 @@ class GildedRoseTest(unittest.TestCase):
         self.assert_brie_sell_in(1, 2, 0)
         self.assert_brie_sell_in(-2, -1, 0)
 
-    def test_elixir(self):
-        self.assert_elixir_quality(6, 5, 7)
-        self.assert_elixir_quality(5, -1, 7)
+    def test_generic(self):
+        self.assert_generic_quality(6, 5, 7)
+        self.assert_generic_quality(5, -1, 7)
+        self.assert_generic_quality(19, 10, 20)
+        self.assert_generic_quality(5, 3, 6)
 
-        self.assert_elixir_sell_in(4, 5, 7)
-        self.assert_elixir_sell_in(-2, -1, 7)
-
-    def test_dexterity_vest(self):
-        self.assert_item_quality(19, DEXTERITY_VEST, 10, 20)
-
-        self.assert_item_sell_in(9, DEXTERITY_VEST, 10, 20)
-
-    def test_mana_cake(self):
-        self.assert_item_quality(5, CONJURED_MANA_CAKE, 3, 6)
-
-        self.assert_item_sell_in(2, CONJURED_MANA_CAKE, 3, 6)
+        self.assert_generic_sell_in(4, 5, 7)
+        self.assert_generic_sell_in(-2, -1, 7)
+        self.assert_generic_sell_in(9, 10, 20)
+        self.assert_generic_sell_in(2, 3, 6)
 
     def test_sulfuras(self):
         self.assert_sulfuras_quality(80, 0, 80)
